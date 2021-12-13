e3e63f5ad937d9b01f11f6850f0271b6c2c7c883
Paul Anthony McGowan <xhostcom@gmail.com>
2021-12-13 05:58:35 +0000

    Gitacora saved 7 changed files

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,6 +6,30 @@ import { definitionsFromContext } from "stimulus/webpack-helpers"
 import StimulusReflex from 'stimulus_reflex'
 import consumer from '../channels/consumer'
 import controller from '../controllers/application_controller'
+// Example request import, is declared in request.d.ts
+import { FetchRequest } from "@rails/request.js"
+import { navigator } from "@hotwired/turbo"
+
+function showProgressBar() {
+    navigator.delegate.adapter.progressBar.setValue(0)
+    navigator.delegate.adapter.progressBar.show()
+}
+function hideProgressBar() {
+    navigator.delegate.adapter.progressBar.setValue(1)
+    navigator.delegate.adapter.progressBar.hide()
+}
+export function withProgress(request) {
+    showProgressBar()
+
+    return request.then((response) => {
+        hideProgressBar()
+        return response
+    })
+}
+export function get(url, options) {
+    const request = new FetchRequest("get", url, options)
+    return withProgress(request.perform())
+}
 
 const application = Application.start()
 const context = require.context("controllers", true, /_controller\.js$/)
