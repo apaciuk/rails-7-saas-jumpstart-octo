eb91311c7088c5480daa340e3af6b128dcb3104d
xhostcom <xhostcom@gmail.com>
2021-12-21 02:35:11 +0000

    Gitacora saved 7 changed files

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,5 +7,8 @@ pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
+pin 'md5', to: 'https://cdn.skypack.dev/md5'
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.1.3/dist/js/bootstrap.esm.js"
 pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.0/lib/index.js"
+pin "trix"
+pin "@rails/actiontext", to: "actiontext.js"
