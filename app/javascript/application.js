692ffd239e22f770c1b3c2946433ff0ae3e461ae
xhostcom <xhostcom@gmail.com>
2021-12-21 02:43:07 +0000

    Gitacora saved 15 changed files

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import '@hotwired/turbo-rails'
+import 'controllers'
 import 'bootstrap'
 //import * as bootstrap from "bootstrap"
-import "trix"
-import "@rails/actiontext"
+import 'trix'
+import '@rails/actiontext'
