f9339dc39dc9847b1531938236115db6554008ce
xhostcom <xhostcom@gmail.com>
2021-12-21 02:38:08 +0000

    Gitacora saved 7 changed files

--- app/javascript/controllers/md5_controller.js
+++ app/javascript/controllers/md5_controller.js
@@ -1,7 +1,8 @@
 import { Controller } from "@hotwired/stimulus"
-
+// md5 example
 export default class extends Controller {
-    connect() {
-        this.element.textContent = "Hello World!"
+    convert(event) {
+        event.preventDefault()
+        event.target.textContent = md5(event.target.textContent)
     }
 }
