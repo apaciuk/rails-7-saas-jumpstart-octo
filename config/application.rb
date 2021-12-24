246c3e03a64d49af375bec4726ba8121220f00f0
xhostcom <xhostcom@gmail.com>
2021-12-24 12:20:27 +0000

    lib autoload path

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module Rails7SaasJumpstart
     config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
-    #
+    config.autoload_paths << Rails.root.join('lib')
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
