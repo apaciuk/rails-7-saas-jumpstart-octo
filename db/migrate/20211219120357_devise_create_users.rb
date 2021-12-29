c7924bf93257cdec9300140417033d513a8a66c8
xhostcom <xhostcom@gmail.com>
2021-12-29 14:04:05 +0000

    Edit

--- db/migrate/20211219120357_devise_create_users.rb
+++ db/migrate/20211219120357_devise_create_users.rb
@@ -4,7 +4,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0] # rubocop:todo Style/Docu
   def change # rubocop:todo Metrics/AbcSize, Metrics/MethodLength
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ''
+      # t.string :username, null: false, default: ''
+      t.string :email, null: false, default: ''
       t.string :encrypted_password, null: false, default: ''
 
       ## Recoverable
@@ -39,10 +40,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0] # rubocop:todo Style/Docu
 
       t.timestamps null: false
     end
-
-    add_index :users, :email,                unique: true
+    # add_index :users, :username, unique: true
+    add_index :users, :email, unique: true
     add_index :users, :reset_password_token, unique: true
     add_index :users, :confirmation_token,   unique: true
-    # add_index :users, :unlock_token,         unique: true
+    # add_index :users, :unlock_token, unique: true
   end
 end
