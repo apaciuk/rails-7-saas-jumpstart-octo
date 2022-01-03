a87fd58bb3a6706a714c82eab269254957716537
xhostcom <xhostcom@gmail.com>
2022-01-03 18:05:05 +0000

    Update

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import '@hotwired/turbo-rails'
-import 'controllers'
-import * as bootstrap from "bootstrap"
+import "bootstrap"
 import 'trix'
 import '@rails/actiontext'
 import "@rails/request.js"
