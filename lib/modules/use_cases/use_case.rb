8f0ef32e1baec218099df01367edcc4711051646
xhostcom <xhostcom@gmail.com>
2022-01-08 12:02:14 +0000

    Use Cases

---
+++ lib/modules/use_cases/use_case.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module UseCase
+  extend ActiveSupport::Concern
+  include ActiveModel::Validations
+
+  module ClassMethods
+    # The perform method of a UseCase should always return itself
+    def perform(*args)
+      new(*args).tap { |use_case| use_case.perform }
+    end
+  end
+
+  # implement all the steps required to complete this use case
+  def perform
+    raise NotImplementedError
+  end
+
+  # inside of perform, add errors if the use case did not succeed
+  def success?
+    errors.none?
+  end
+end
