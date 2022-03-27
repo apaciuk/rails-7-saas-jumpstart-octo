090e6996532107d272866ca406af0d0cd2a2e051
xhostcom <xhostcom@gmail.com>
2022-03-27 10:57:06 +0100

    Gitacora saved 3 changed files

--- db/migrate/20220327091024_create_user_role.rb
+++ db/migrate/20220327091024_create_user_role.rb
@@ -1,7 +1,7 @@
 class CreateUserRole < ActiveRecord::Migration[7.0]
   def change
     create_table :user_roles do |t|
-      t.string :role, null: true, default: :user
+      t.string :role
       t.references :user, null: true, foreign_key: true
 
       t.timestamps
