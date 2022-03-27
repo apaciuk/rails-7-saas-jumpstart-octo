801664d4389fc6b7650c1c983deb4774ac1d7f42
xhostcom <xhostcom@gmail.com>
2022-03-27 17:02:19 +0100

    Gitacora saved 1 changed files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base # rubocop:todo Style/Documentation
+  include Pundit::Authorization 
   include PunditRoles
 
   protect_from_forgery with: :exception
