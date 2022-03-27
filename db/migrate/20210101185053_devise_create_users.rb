6abbf6141ba7799c404cc68485bea0055307e4b0
xhostcom <xhostcom@gmail.com>
2022-03-27 10:36:21 +0100

    Gitacora saved 3 changed files

--- db/migrate/20210101185053_devise_create_users.rb
+++ db/migrate/20210101185053_devise_create_users.rb
@@ -37,7 +37,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0] # rubocop:todo Style/Docu
       t.string :last_name
       t.datetime :announcements_last_read_at
       t.boolean :admin, default: false
-      t.integer :user_role_id, unique: true,  null: false, default: 0
+      t.integer :user_role_id, unique: true,  null: true, default: null
       t.timestamps null: false
     end
     # add_index :users, :username, unique: true
