8fce43a4b7e12104bfc73cfdce58807d5be2d6a4
xhostcom <xhostcom@gmail.com>
2022-03-29 22:26:36 +0100

    Gitacora saved 1 changed files

--- db/migrate/20210101185053_devise_create_users.rb
+++ db/migrate/20210101185053_devise_create_users.rb
@@ -33,7 +33,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0] # rubocop:todo Style/Docu
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at 
 
-      t.integer :role, default: 0, null: false
+      t.integer :role, default: 0, null: false 
 
       t.string :first_name
       t.string :last_name
