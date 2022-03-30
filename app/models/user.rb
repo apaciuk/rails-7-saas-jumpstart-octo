41c34753e02d33db18bc18185131cde55c8d94fb
xhostcom <xhostcom@gmail.com>
2022-03-30 09:10:06 +0100

    Gitacora saved 1 changed files

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,10 @@ class User < ApplicationRecord
   devise :masqueradable, :database_authenticatable, :confirmable, :registerable, :trackable, :recoverable,
          :rememberable, :validatable, :omniauthable 
   # Roles
-  enum role:{ user: 0, member: 1 } # add other roles as req
+  enum role:{
+      user: 0, 
+      member: 1 
+  }, _prefix: true # add other roles as req
   after_initialize :set_default_role, if: :new_record?
   def set_default_role 
      self.role ||= :user
