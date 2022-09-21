8786af89f4aefd774507b37fcd8c19f0d79dbf23
Paul Anthony McGowan <xhostcom@aol.com>
2022-09-21 13:04:42 +0100

    update

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module JumpstartOcto
   class Application < Rails::Application # rubocop:disable Style/Documentation
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    config.assets.initialize_on_precompile = false
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
